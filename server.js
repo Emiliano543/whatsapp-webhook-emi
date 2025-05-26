185bbe05f4327ad224502df2278f425de1e41fbf
Emiliano543 <prusia_555@hotmail.com>
2025-05-26 19:02:17 -0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 10000;
-app.use(express.json());
-app.post('/webhook', (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
-});
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
