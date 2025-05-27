27d47861a28bb760cd8440ebbd92484ddd457c8e
Emiliano543 <prusia_555@hotmail.com>
2025-05-27 00:32:08 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 VERIFY_TOKEN = "Emi-token-123"
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def webhook():
-    if request.method == 'GET':
-        mode = request.args.get('hub.mode')
-        token = request.args.get('hub.verify_token')
-        challenge = request.args.get('hub.challenge')
-        if mode == 'subscribe' and token == VERIFY_TOKEN:
+    if request.method == "GET":
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        if mode == "subscribe" and token == VERIFY_TOKEN:
             return challenge, 200
         else:
-            return 'Forbidden', 403
-    elif request.method == 'POST':
-        data = request.json
-        print(f"Mensaje recibido: {data}")
-        return 'EVENT_RECEIVED', 200
+            return "Unauthorized", 403
+    elif request.method == "POST":
+        data = request.get_json()
+        print("Mensaje recibido:", data)
+        return "EVENT_RECEIVED", 200
 
-    return 'Method Not Allowed', 405
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
