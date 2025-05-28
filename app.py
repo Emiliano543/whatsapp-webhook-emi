823448c645970c1a4babdcb6e8c623a6c0941d21
Emiliano543 <prusia_555@hotmail.com>
2025-05-28 02:51:40 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "Emi-token-123"
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Webhook activo", 200
+
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return challenge, 200
+        else:
+            return "Error de verificación", 403
+    if request.method == "POST":
+        print("Mensaje recibido:", request.json)
+        return "Evento recibido", 200
